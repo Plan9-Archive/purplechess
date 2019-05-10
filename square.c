7d90e28f2af042625729295b70543a231c8da7f0
glenda <glenda@opti>
2019-05-10 13:57:51 -0500

    make hexagram visible in dark view mode

--- square.c
+++ square.c
@@ -209,18 +209,20 @@ redraw(Square *s)
 	}
 	switch(s->line){
 	case 1:
+		color=visflag < 3 ? black : whtpc;
 		targ.x = s->r.min.x;
 		targ.y = s->r.min.y + ((s->r.max.y - s->r.min.y) / 2);
 		dest.x = s->r.max.x;
 		dest.y = targ.y;
-		line(screen, targ, dest, 0, 0, 4, blkpc, targ);
+		line(screen, targ, dest, 0, 0, 4, color, targ);
 		break;
 	case 2:
+		color=visflag < 3 ? black : whtpc;
 		targ.x = s->r.min.x;
 		targ.y = s->r.min.y + ((s->r.max.y - s->r.min.y) / 2);
 		dest.x = s->r.max.x;
 		dest.y = targ.y;
-		line(screen, targ, dest, 0, 0, 4, blkpc, targ);
+		line(screen, targ, dest, 0, 0, 4, color, targ);
 		ell1=(s->r.max.x - s->r.min.x) / 2;
 		ell2=(s->r.max.y - s->r.min.y) / 2;
 		targ.x = s->r.min.x + ell1;
@@ -228,25 +230,25 @@ redraw(Square *s)
 		if(ell2 < ell1)
 			ell1 = ell2;
 		ell1 = ell1 - 25;
-		color=blkpc;
 		ellipse(screen, targ, ell1, ell1, 4, color, targ);
 		break;
 	case 3:
+		color=visflag < 3 ? black : whtpc;
 		targ.x = s->r.min.x;
 		targ.y = s->r.min.y + ((s->r.max.y - s->r.min.y) / 2);
 		dest.x = s->r.max.x;
 		dest.y = targ.y;
-		line(screen, targ, dest, 0, 0, 4, blkpc, targ);
+		line(screen, targ, dest, 0, 0, 4, color, targ);
 		targ.x = s->r.min.x + ((s->r.max.x - s->r.min.x) / 4);
 		targ.y = s->r.min.y + ((s->r.max.y - s->r.min.y) / 4);
 		dest.x = s->r.max.x - ((s->r.max.x - s->r.min.x) / 4);
 		dest.y = s->r.max.y - ((s->r.max.y - s->r.min.y) / 4);
-		line(screen, targ, dest, 0, 0, 4, blkpc, targ);
+		line(screen, targ, dest, 0, 0, 4, color, targ);
 		targ.x = s->r.min.x + ((s->r.max.x - s->r.min.x) / 4);
 		targ.y = s->r.max.y - ((s->r.max.y - s->r.min.y) / 4);
 		dest.x = s->r.max.x - ((s->r.max.x - s->r.min.x) / 4);
 		dest.y = s->r.min.y + ((s->r.max.y - s->r.min.y) / 4);
-		line(screen, targ, dest, 0, 0, 4, blkpc, targ);
+		line(screen, targ, dest, 0, 0, 4, color, targ);
 		break;
 	}
 	if(s->drawhexa == 1){
